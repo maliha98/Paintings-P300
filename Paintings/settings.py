99a34e5352e7a130a01b55219d986d156be248b9
maliha98 <malihaztabassumz@gmail.com>
2020-10-08 19:34:22 +0600

    templates added

--- Paintings/settings.py
+++ Paintings/settings.py
@@ -119,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR, "static"
+]
+
+MEDIA_URL = '/images/'
+MEDIA_ROOT = (
+   BASE_DIR, 'static/images'
+)
--- home/views.py
+++ home/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def homeView(request):
-    return render(request,'index.html')
+    return render(request,'index.html',{})
+
+
+def profileView (request):
+    return render(request,'profile.html')
+
+def aboutView (request):
+    return render(request, 'about.html')
