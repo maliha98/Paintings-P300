935983638917147af441c84fa9fe7a89937d91c2
maliha98 <malihaztabassumz@gmail.com>
2020-10-21 21:04:56 +0600

    signin system added

--- Paintings/urls.py
+++ Paintings/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from home.views import *
+from user_accounts.views import *
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -8,5 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homeView, name='home'),
     path('about/', aboutView, name='about'),
+    path('signin/', signInPage, name='signin'),
+    path('logout/', logoutPage, name='logout'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ user_accounts/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User, auth
+
+
+def signInPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'signin.html')
+
+
+def logoutPage(request):
+    logout(request)
+    return redirect('signin')
