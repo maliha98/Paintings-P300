b3afc3496604c4001df7574a24ac49fa11295d62
maliha98 <malihaztabassumz@gmail.com>
2020-10-23 16:34:22 +0600

    cart

--- home/models.py
+++ home/models.py
@@ -1,2 +1,17 @@
 from django.db import models
+from product.models import Product
+from django.contrib.auth.models import User
 
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, null=True, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    purchased = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.product.title
+
+    def productTotal(self):
+        t = self.product.price * self.quantity
+        return t
--- product/models.py
+++ product/models.py
@@ -13,9 +13,8 @@ class Product(models.Model):
     title = models.CharField(max_length=100, null=True)
     description = models.CharField(max_length=1000, null=True)
     product_image = models.ImageField(default="product.jpg", null=True)
-    category = models.ForeignKey(
-        Category, null=True, on_delete=models.CASCADE)
-    price = models.FloatField('$', null=True)
+    category = models.ForeignKey(Category, null=True, on_delete=models.CASCADE)
+    price = models.FloatField('Price : $', null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
